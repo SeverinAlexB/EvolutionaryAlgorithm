48521625af3012db77f014ffb467aaf3d6bd3636
Severin Bühler <sevi_buehler@hotmail.com>
2014-11-30 15:08:59 +0100

    no message

--- src/ch/hsr/sevi/ea/EsimException.java
+++
@@ -1,8 +0,0 @@
-package ch.hsr.sevi.ea;
-
-public class EsimException extends Exception {
-	private static final long serialVersionUID = 1L;
-	public EsimException(String msg){
-		super(msg);
-	}
-}
